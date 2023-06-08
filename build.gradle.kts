d908ca5cb13de9db5a4242e3685500e3644b17ff
Flavia Podariu <flaviapodariu@gmail.com>
2023-06-08 14:57:10 +0300

    Added main class to manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,12 @@ repositories {
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.licenta.Application.kt"
+    }
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
