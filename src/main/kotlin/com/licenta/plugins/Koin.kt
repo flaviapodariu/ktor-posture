09a4eaffd1cc2ace130deacce1bf39b1c314c29e
Flavia Podariu <flaviapodariu@gmail.com>
2023-06-09 17:32:16 +0300

    Changed db credentials

--- src/main/kotlin/com/licenta/plugins/Koin.kt
+++ src/main/kotlin/com/licenta/plugins/Koin.kt
@@ -22,7 +22,7 @@ fun Application.configureKoin() {
 val postureModule = module {
     single {
         Database.connect(
-            url= "jdbc:mysql://localhost:3306/posture-improver",
+            url= "jdbc:mysql://containers-us-west-82.railway.app:6193/railway",
             user= "root",
             password = System.getenv("DB_PASSWORD")
         )
